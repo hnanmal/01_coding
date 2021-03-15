4a4e2809fdfb03cec54830ca665ca3e13521c61f
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-03-15 20:00:28 +0900

    Update mksvbot1.py

--- mksvbot1.py
+++ mksvbot1.py
@@ -47,7 +47,7 @@ async def widearmjump(ctx, count):
     bIs_dude = random.randint(0,1)
     for i in range(count_int - 1 + bIs_dude):
         await ctx.send(f"{i+1}!")
-        await asyncio.sleep(3.0)
+        await asyncio.sleep(1.0)
     if bIs_dude == 1:
         await ctx.send("정신을 못차렸는지 마지막 구호를 외쳤습니다;; 처음부터 다시!...")
         await widearmjump(ctx, count)
