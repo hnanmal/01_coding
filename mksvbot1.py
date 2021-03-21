1b8803be046ba4b7392550c99b1f3e523fe92582
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-03-21 19:21:58 +0900

    Update mksvbot1.py

--- mksvbot1.py
+++ mksvbot1.py
@@ -122,8 +122,8 @@ async def weather(ctx):
     crnt_sensible = soup_num[2].string
     crnt_rainfall = soup_num[3].string
     embd_wetr = discord.Embed(title="**날씨...**", description="현재의 날씨 정보를 출력합니다...", color=0x62c1cc)
-    embd_wetr.add_field(name="현재 기온", value=f"`{crnt_temp} ℃`", inline=False)
-    embd_wetr.add_field(name="체감 온도", value=f"`{crnt_sensible} ℃`", inline=False)
+    embd_wetr.add_field(name="현재 기온", value=f"`{crnt_temp} ℃`", inline=True)
+    embd_wetr.add_field(name="체감 온도", value=f"`{crnt_sensible} ℃`", inline=True)
     embd_wetr.add_field(name="시간당 강수량", value=f"`{crnt_rainfall} mm/hr`", inline=False)
     await ctx.send(embed=embd_wetr)
     pass
