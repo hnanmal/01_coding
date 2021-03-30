492ce8d6e549f74e89fa9c98b89e5c3389fe5504
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-03-31 00:52:45 +0900

    s

--- 1.py
+++ 1.py
@@ -1,1 +1,46 @@
-from oauth2client.client import flow_from_clientsecrets
+
+import asyncio
+from asyncio.tasks import sleep
+import discord
+from discord.ext import commands
+import sys
+import random
+import urllib.request
+from bs4 import BeautifulSoup
+import json
+from urllib import parse
+from urllib.parse import quote_plus
+from collections import OrderedDict
+
+input = "미성동"
+defaultUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query='
+plusUrl = str(input) + '날씨'
+basic_url = str(defaultUrl) + str(quote_plus(plusUrl))
+fp = urllib.request.urlopen(str(basic_url))
+source = fp.read()
+fp.close()
+soup = BeautifulSoup(source, 'html.parser')
+soup_tmp = soup.findAll("span",class_="todaytemp")
+soup_num = soup.findAll("span",class_="num")
+if len(soup.findAll("span",class_="rainfall")) == 0 :
+    crnt_rain = "None"
+else:
+    soup_rain = soup.findAll("span",class_="rainfall")
+    crnt_rain = soup_rain[0].text
+
+if len(soup.findAll("span",class_="indicator")) == 0 :
+    crnt_uv = "None"
+else:
+    soup_uv = soup.findAll("span",class_="indicator")
+    crnt_uv = soup_uv[0].text
+
+soup_detail = soup.findAll("div",class_="detail_box")
+crnt_temp = soup_tmp[0].string
+crnt_sensible = soup_num[2].string
+crnt_detail = soup_detail[0].text
+crnt_detail_str = crnt_detail.split(" ")
+detail_1 = crnt_detail_str[2]+ " " + crnt_detail_str[3]
+detail_2 = crnt_detail_str[4]+ " " + crnt_detail_str[5]
+detail_3 = crnt_detail_str[6]+ " " + crnt_detail_str[7]
+
+print(crnt_uv)
--- mksvbot1.py
+++ mksvbot1.py
@@ -136,8 +136,19 @@ async def weather(ctx, input):
     soup_detail = soup.findAll("div",class_="detail_box")
     crnt_temp = soup_tmp[0].string
     crnt_sensible = soup_num[2].string
-    crnt_rain = soup_rain[0].text
-    # crnt_uv = soup_uv[0].text
+    
+    if len(soup.findAll("span",class_="rainfall")) == 0 :
+        crnt_rain = "None"
+    else:
+        soup_rain = soup.findAll("span",class_="rainfall")
+        crnt_rain = soup_rain[0].text
+    
+    if len(soup.findAll("span",class_="indicator")) == 0 :
+        crnt_uv = "None"
+    else:
+        soup_uv = soup.findAll("span",class_="indicator")
+        crnt_uv = soup_uv[0].text
+    
     crnt_detail = soup_detail[0].text
     crnt_detail_str = crnt_detail.split(" ")
     detail_1 = crnt_detail_str[2]+ " " + crnt_detail_str[3]
@@ -146,8 +157,9 @@ async def weather(ctx, input):
     embd_wtr = discord.Embed(title=f"**현재 {input}의 날씨는...**", description="해당 지역의 현재 날씨 정보를 출력합니다...", color=0x62c1cc)
     embd_wtr.add_field(name="**현재 기온**", value=f"`{crnt_temp} ℃`", inline=True)
     embd_wtr.add_field(name="**체감 온도**", value=f"`{crnt_sensible} ℃`", inline=True)
-    embd_wtr.add_field(name="**강수현황**", value=f"`{crnt_rain}`", inline=False)
-    # embd_wtr.add_field(name="**자외선**", value=f"`{crnt_uv}`", inline=False)
+    embd_wtr.add_field(name="**강수 현황**", value=f"`{crnt_rain}`", inline=False)
+    embd_wtr.add_field(name="**자외선 현황**", value=f"`{crnt_uv}`", inline=False)
+    
     embd_wtr.add_field(name="**부가정보**", value=f"`{detail_1}`\n`{detail_2}`\n`{detail_3}`", inline=True)
     await ctx.send(embed=embd_wtr)
 
