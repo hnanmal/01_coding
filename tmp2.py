4387f3ad70f198af98aac33cf21bad1e0c3307d6
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-04-10 23:11:33 +0900

    commit on 'master'

--- tmp2.py
+++ tmp2.py
@@ -7,16 +7,59 @@
 #     print(list(many_gen()))
 
 
-def echo(value=None):
-        print("Execution starts when 'next()' is called for the first time.")
-        try:
-            while True:
-                try:
-                    value = (yield value)
-                except Exception as e:
-                    value = e
-        finally:
-            print("Don't forget to clean up when 'close()' is called.")
+# def echo(value=None):
+#         print("Execution starts when 'next()' is called for the first time.")
+#         try:
+#             while True:
+#                 try:
+#                     value = (yield value)
+#                 except Exception as e:
+#                     value = e
+#         finally:
+#             print("Don't forget to clean up when 'close()' is called.")
 
-generator = echo(1)
-print(next(generator))
+# generator = echo(1)
+# print(next(generator))
+
+# a = 'my string'
+# print(type(a))
+# print(dir(a))
+
+# a = 'prac'
+# b = iter(a)
+# print(next(b))
+# print(next(b))
+# print(next(b))
+# print(next(b))
+# # print(next(b))
+
+class prac:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+
+        self.index = self.index-1
+        return self.data[self.index]
+
+a = prac('myData')
+
+for char in a:
+    print(char)
+print("-"*30)
+
+
+def reverse(data):
+    for index in range(len(data)-1,-1,-1):
+        yield data[index]
+
+a = reverse('practice')
+for char in a:
+    print(char)
+    
