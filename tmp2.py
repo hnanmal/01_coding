5c36b095eb2b00df78aed3f5a65f0b80fcc97992
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-04-10 22:48:34 +0900

    Update tmp2.py

--- tmp2.py
+++ tmp2.py
@@ -5,3 +5,18 @@
 #     v = [1, 2, 3, 4]
 #     yield x for x in v
 #     print(list(many_gen()))
+
+
+def echo(value=None):
+        print("Execution starts when 'next()' is called for the first time.")
+        try:
+            while True:
+                try:
+                    value = (yield value)
+                except Exception as e:
+                    value = e
+        finally:
+            print("Don't forget to clean up when 'close()' is called.")
+
+generator = echo(1)
+print(next(generator))
